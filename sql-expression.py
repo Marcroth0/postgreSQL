93c02b29fa48b6e0dae595ce6e528896f28b6cc7
Marcroth0 <marcusrothoff@hotmail.com>
2021-12-19 22:16:14 +0000

    pushing

---
+++ sql-expression.py
@@ -0,0 +1,17 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, Metadata
+)
+
+# executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta, 
+)
+
+# making the connection
+with db.connect() as connection:
+    
